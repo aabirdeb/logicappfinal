9d9b54cd1246b7702b984a94968e07513db86154
aabirdeb <103592645+aabirdeb@users.noreply.github.com>
2023-03-28 16:23:56 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -20,9 +20,4 @@ resource "azurerm_virtual_network" "rgvnet" {
   resource_group_name = azurerm_resource_group.rg.name
 
 
-subnet {
-    name           = "subnet1"
-    address_prefix = "10.0.1.0/24"
-  }
-
   
