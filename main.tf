6f57b2b39aade3786710c0f633efd6abf349f842
aabirdeb <103592645+aabirdeb@users.noreply.github.com>
2023-03-28 16:23:44 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -25,10 +25,4 @@ subnet {
     address_prefix = "10.0.1.0/24"
   }
 
-  subnet {
-    name           = "subnet2"
-    address_prefix = "10.0.2.0/24"
-    security_group = azurerm_network_security_group.ns.id
-  }
   
-}
