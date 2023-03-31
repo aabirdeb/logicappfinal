800b3f176879968617a43389d26dcf8af81ef9f2
aabirdeb <103592645+aabirdeb@users.noreply.github.com>
2023-03-31 15:18:46 +0530

    Update output.tf

--- output.tf
+++ output.tf
@@ -1,3 +1,8 @@
 output "resource_group_name" {
   value = azurerm_resource_group.rg.name
 }
+
+
+#nice work
+#place all the outupt required in this project 
+#you have yo mention all the variables in the allocated resource otherwise a error will be placed in the module so be carefull what you do  in theis category
